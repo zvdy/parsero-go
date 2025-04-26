bc7dc4e1831c68d7e5a48205b37e76edfe764b0e
zvdy <cristian.030902@gmail.com>
2025-04-26 22:17:33 +0200

    test: add search engine testing functionality

---
+++ internal/search/search_test.go
@@ -0,0 +1,12 @@
+package search
+
+import "testing"
+
+func TestSearchFunctionality(t *testing.T) {
+    // Add your test cases here
+    t.Run("example test", func(t *testing.T) {
+        if 1+1 != 2 {
+            t.Errorf("Expected 2, got %d", 1+1)
+        }
+    })
+}
